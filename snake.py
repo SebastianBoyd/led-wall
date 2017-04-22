e2052a976a814d39f94e81c4803b18835afebc32
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2017-04-22 15:41:47 -0700

    Web control

--- snake.py
+++ snake.py
@@ -1,5 +1,8 @@
 from main import *
 import random
+from flask import Flask
+import time
+app = Flask(__name__)
 
 datapins = [13, 16, 22, 24, 25, 5, 18, 4, 17, 23]
 clockpin = 27
@@ -10,11 +13,13 @@ class Snake:
     snake_color = 0x00FFFF
     apple_color = 0xFF00FF
     direction = (0, 1) # (x, y)
+    url = "/snake"
     def __init__(self, screen):
         self.screen = screen
         self.screen.clear()
         self.add_point(self.random_location(padding=2))
         self.add_apple()
+        self.setup_server()
     def add_point(self, coordinates):
         self.snake.append(coordinates)
         self.screen.set_pixel(coordinates, self.snake_color)
@@ -49,20 +54,49 @@ class Snake:
     def remove_apple(self, coordinates):
         self.apples.remove(coordinates)
         self.screen.set_pixel(coordinates, self.snake_color)
+    def setup_server(self):
+        @app.route(self.url + "/up")
+        def up():
+            self.set_dir((0, -1))
+            return "ok"
+        @app.route(self.url + "/down")
+        def down():
+            self.set_dir((0, 1))
+            return "ok"
+        @app.route(self.url + "/left")
+        def left():
+            self.set_dir((-1, 0))
+            return "ok"
+        @app.route(self.url + "/right")
+        def right():
+            self.set_dir((1, 0))
+            return "ok"
+        @app.route(self.url + "/move")
+        def move_url():
+            self.move()
+            return "ok"
 
-screen = LedScreen(datapins, clockpin, brightness=100)
 
-screen.display_image('test-img.png')
-time.sleep(1)
-s = Snake(screen)
-while True:
-    cmd = raw_input()
-    if cmd == 'w':
-        s.set_dir((0, -1))
-    elif cmd == 'a':
-        s.set_dir((-1, 0))
-    elif cmd == 's':
-        s.set_dir((0, 1))
-    elif cmd == 'd':
-        s.set_dir((1, 0))
-    s.move()
+
+# while True:
+#     cmd = raw_input()
+#     if cmd == 'w':
+#         s.set_dir((0, -1))
+#     elif cmd == 'a':
+#         s.set_dir((-1, 0))
+#     elif cmd == 's':
+#         s.set_dir((0, 1))
+#     elif cmd == 'd':
+#         s.set_dir((1, 0))
+#     s.move()
+@app.route("/snake")
+def controller():
+    return app.send_static_file('snake_controller.html')
+
+if __name__ == "__main__":
+    print('run')
+    screen = LedScreen(datapins, clockpin, brightness=100)
+    screen.display_image('test-img.png')
+    time.sleep(1)
+    s = Snake(screen)
+    app.run(host="0.0.0.0", port=int("5000"))
