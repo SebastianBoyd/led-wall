0cdb3b27497e1dac2c49d26ce0c05ba0eb63eba4
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2017-08-10 23:09:32 -0700

    Add features

---
+++ clear.py
@@ -0,0 +1,4 @@
+import main
+
+screen = main.LedScreen(brightness=255)
+screen.clear()
---
+++ stripes.py
@@ -0,0 +1,6 @@
+import main
+import time
+
+delay = 0.1
+screen = main.LedScreen(brightness=255)
+screen.display_image('stripes.png')
